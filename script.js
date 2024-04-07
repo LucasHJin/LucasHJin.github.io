92d84a4c58384a3464dd9ef1dc35bc39697204d6
Lucas Jin <kblazer20@gmail.com>
2024-04-07 10:43:39 -0400

    Created template for page and added typed text
    This provides the basic framework to be built off of to create the full website.

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".auto-type", {
+        strings: ["^250 Hello, World!", "^250 Hi!", "^250 Hello!", "^250 Welcome!", "^250 Bonjour!", "^250 Salut!", "^250 你好", "^250 こんにちは", "^250 안녕하세요"],
+        typeSpeed: 300,
+        backSpeed: 300,
+        loop: true
+    });
+});
