6410a05fc1aa29f89b9a36053e14644833d19d20
Lucas Jin <kblazer20@gmail.com>
2024-04-08 16:45:38 -0400

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//using typing library 
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed(".auto-type", {
         strings: ["^200 Hello, World!", "^200 Hi!", "^200 Hello!", "^200 Welcome!", "^200 Bonjour!", "^200 Salut!", "^200 你好", "^200 こんにちは", "^200 안녕하세요"],
