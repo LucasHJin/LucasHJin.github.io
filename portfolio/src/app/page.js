1a12b7f235b4b490aad6513412cb7f352933b357
Lucas Jin <kblazer20@gmail.com>
2025-03-31 13:17:19 -0400

    Italic styling

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Hi my name is lucas!
       <p>Nice to meet you</p>
       <p className="bold">Nice to meet you</p>
+      <p className="link">Nice to meet you</p>
     </div>
   );
 }
