491bc558478f74b87ecd4be886cad2b5053ae9d4
Lucas Jin <kblazer20@gmail.com>
2025-03-31 22:21:47 -0400

    Basic header, footer, new color scheme

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,14 +1,33 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"; // Applies global css to everything
+import Link from "next/link";
+
+// add a middle underline appear
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <header>
-
+          <nav>
+            <ul>
+              <li><Link href="/">lucas jin</Link></li>
+              <li><Link href="/">about</Link></li>
+              <li><Link href="/projects">projects</Link></li>
+              <li><Link href="/zettelkasten">zettlekasten</Link></li>
+            </ul>
+          </nav>
         </header>
-        {children}
+        <main>{children}</main>
+        <footer>
+        <nav>
+            <ul>
+              <li><Link href="/">insta</Link></li>
+              <li><Link href="/">email</Link></li>
+              <li><Link href="/">linkedin</Link></li>
+            </ul>
+          </nav>
+        </footer>
       </body>
     </html>
   );
