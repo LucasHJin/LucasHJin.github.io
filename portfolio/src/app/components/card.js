3a4e03cb7e2d4d78e26d5d2ca3abcc13462c938b
Lucas Jin <kblazer20@gmail.com>
2025-04-08 14:05:20 -0400

    Made card component reusable

--- portfolio/src/app/components/card.js
+++ portfolio/src/app/components/card.js
@@ -4,37 +4,48 @@ import { FaGithub } from "react-icons/fa";
 import { SiDevpost } from "react-icons/si";
 import { AiOutlineExport } from "react-icons/ai";
 import Link from "next/link";
+import React from 'react';
+import PropTypes from 'prop-types';
 
+// make it only image (vertical), appear down when hovered
 
-export default function Card() {
+export default function Card( { title, description, image, links, tags } ) {
     return (
       <div className="outer-container card">
         <div className="container">
           <div className="item image">
-            <img src="../static/frc_dcmp_2025.JPG"></img>
+            <img src={image}></img>
           </div>
           <div className="other">
             <div className="top">
-              <div className="item name">
-                <h3>Name</h3>
+              <div className="item title">
+                <h3>{title}</h3>
               </div>
               <div className="item links">
-                <Link href="https://github.com/LucasHJin" target="_blank" rel="noopener noreferrer" className="link">
-                  <FaGithub />
-                </Link>
-                <Link href="https://devpost.com" target="_blank" rel="noopener noreferrer" className="link">
-                  <SiDevpost />
-                </Link>
-                <Link href="https://google.com" target="_blank" rel="noopener noreferrer" className="link">
-                  <AiOutlineExport />
-                </Link>
+                {links?.github && 
+                  <Link href={links.github} target="_blank" rel="noopener noreferrer" className="link">
+                    <FaGithub />
+                  </Link>
+                }
+                {links?.devpost && 
+                  <Link href={links.devpost} target="_blank" rel="noopener noreferrer" className="link">
+                    <SiDevpost />
+                  </Link>
+                }
+                {links?.published && 
+                  <Link href={links.published} target="_blank" rel="noopener noreferrer" className="link">
+                    <AiOutlineExport />
+                  </Link>
+                }
               </div>
             </div>
             <div className="item description">
-              <p>descriptions</p>
+              <p>{description}</p>
             </div>
             <div className="item tags">
-              <Tag></Tag>
+              {tags !== undefined && tags.length > 0 && tags.map((tag, index) => (
+                <Tag key={index} tag={tag} />
+              ))}
             </div>
           </div>
         </div>
@@ -42,3 +53,26 @@ export default function Card() {
     );
   }
   
+Card.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  links: PropTypes.shape({
+    github: PropTypes.string,
+    devpost: PropTypes.string,
+    published: PropTypes.string
+  }),
+  tags: PropTypes.arrayOf(PropTypes.string)
+};
+
+Card.defaultProps = {
+  title: "Default",
+  description: "Default",
+  image: "../../../static/frc_dcmp_2025.JPG",
+  links: {
+    github: null,
+    devpost: null,
+    published: null,
+  },
+  tags: []
+};
--- portfolio/src/app/components/tag.js
+++ portfolio/src/app/components/tag.js
@@ -1,10 +1,19 @@
 import "../styling/tag.css"
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Tag() {
+export default function Tag({ tag }) {
     return (
       <div>
-        <p className="tag">tag</p>
+        <p className="tag">{tag}</p>
       </div>
     );
   }
   
+Tag.propTypes = {
+  tags: PropTypes.string
+}
+
+Tag.defaultProps = {
+  tags: ""
+}
