79c7402e2c83d22535eab6e060dac2493976e3ed
Lucas Jin <kblazer20@gmail.com>
2025-09-10 09:47:13 -0400

    Basic page layout

--- README.md
+++
@@ -1,3 +0,0 @@
-# Hey there!
-
-This is codebase for my personal website. Feel free to browse around!!
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,53 @@
+"use client";
+import { useState } from "react";
+import About from "./components/about";
+import Portfolio from "./components/portfolio";
+import Experiences from "./components/experiences";
+import Other from "./components/other";
 
 export default function Home() {
+  const [active, setActive] = useState(null);
+
+  const sections = [
+    {
+      title: "LUCAS JIN",
+      content: About,
+    },
+    {
+      title: "PORTFOLIO",
+      content: Portfolio,
+    },
+    {
+      title: "EXPERIENCES",
+      content: Experiences,
+    },
+    { title: "OTHER", content: Other },
+  ];
+
   return (
-    <div>
-      LUCAS
+    <div className="container">
+      {sections.map((section, i) => {
+        let className = "section default";
+        if (active === i) {
+          className = "section active";
+        } else if (active !== null) {
+          className = "section inactive";
+        }
+
+        return (
+          <div
+            key={i}
+            className={className}
+            onMouseEnter={() => setActive(i)}
+            onMouseLeave={() => setActive(null)}
+          >
+            <h2 className="vertical-text">{section.title}</h2>
+            <div className="content">
+              <section.content />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
