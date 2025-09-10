063a675f3dbdcc864e44f1529beb253bfea376a9
Lucas Jin <kblazer20@gmail.com>
2025-09-10 11:12:26 -0400

    More styling for about

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -2,88 +2,86 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div>
-      <div className="about">
-        <div className="description">
-          <h3>who am i?</h3>
-          <ul className="text">
-            <li className="italic">aspiring software engineer</li>
-            <li>
-              <a
-                href="https://cs.uwaterloo.ca/"
-                className="important"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                cs @ University of Waterloo
-              </a>
-            </li>
-          </ul>
-          <h3>what else?</h3>
-          <ul className="text">
-            <li>
-              ❤️ robotics (shoutout to{" "}
-              <a
-                href="https://syrc.ca/"
-                className="important"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                frc 8729
-              </a>
-              ), hackathons, and building things
-            </li>
-            <li>hobbies: working out, manhwa and anime</li>
-          </ul>
-          <h3>recent!</h3>
-          <ul className="text">
-            <li>
-              won 🥇 + $1K @{" "}
-              <a
-                href="https://www.hack404.dev/"
-                className="important"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Hack404
-              </a>
-            </li>
-            <li>
-              solo best pitch @{" "}
-              <a
-                href="https://www.terrahacks.ca/"
-                className="important"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Terrahacks
-              </a>
-            </li>
-            <li>
-              released{" "}
-              <a
-                href="https://obsidian.md/plugins?id=pixel-pets"
-                className="important"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                pixel pets
-              </a>{" "}
-              - 800+ downloads
-            </li>
-          </ul>
-        </div>
+    <div className="about">
+      <div className="description">
+        <h3>who am i?</h3>
+        <ul className="text">
+          <li className="italic">aspiring software engineer</li>
+          <li>
+            <a
+              href="https://cs.uwaterloo.ca/"
+              className="important"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              cs @ University of Waterloo
+            </a>
+          </li>
+        </ul>
+        <h3>what else?</h3>
+        <ul className="text">
+          <li>
+            ❤️ robotics (shoutout to{" "}
+            <a
+              href="https://syrc.ca/"
+              className="important"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              frc 8729
+            </a>
+            ), hackathons, and building things
+          </li>
+          <li>hobbies: working out, manhwa and anime</li>
+        </ul>
+        <h3>recent!</h3>
+        <ul className="text">
+          <li>
+            won 🥇 + $1K @{" "}
+            <a
+              href="https://www.hack404.dev/"
+              className="important"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Hack404
+            </a>
+          </li>
+          <li>
+            solo best pitch @{" "}
+            <a
+              href="https://www.terrahacks.ca/"
+              className="important"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terrahacks
+            </a>
+          </li>
+          <li>
+            released{" "}
+            <a
+              href="https://obsidian.md/plugins?id=pixel-pets"
+              className="important"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Pixel Pets
+            </a>{" "}
+            - 800+ downloads
+          </li>
+        </ul>
       </div>
       <div className="more">
         <div className="resume-github">
           <h3>see my...</h3>
         </div>
-         <figure className="image-me">
+        <figure className="image-me">
           <Image
             src="/frc_dcmp_2025.JPG"
             alt="lucas as a part of the drive team at dcmp 2025"
-            width={1200}
-            height={800}
+            width={100}
+            height={100}
           ></Image>{" "}
           {/* approx width and height for scaling */}
           <figcaption>part of frc 8729&apos;s drive team at dcmp!</figcaption>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,4 @@
-import { Montserrat } from "next/font/google";
 import "./styling/globals.css";
-
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  style: ["normal", "italic"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -17,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${montserrat.variable} dark`}
+        className={`dark`}
       >
         {children}
       </body>
